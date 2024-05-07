ef55ef15a33c043d6c427ce30784f79f6a510398
M1rad0 <kainrak312@gmail.com>
2024-05-07 18:12:11 +0300

    Création du FrontController
    Le FrontController est le servlet qui gèrera toutes les requêtes client

---
+++ src/mg/itu/framework/controller/FrontController.java
@@ -0,0 +1,26 @@
+package mg.itu.framework.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FrontController extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("Page : "+req.getRequestURL());
+    }
+}
