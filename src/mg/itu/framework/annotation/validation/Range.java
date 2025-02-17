d28cc3c169a5e641f719253feb2e41dce116b725
Mirado <kainrak312@gmail.com>
2025-02-18 00:18:57 +0300

    sprint15-2741 - Authentification
    Correction annotations sprint14 pour annotation avec classe et non string
    Authentification en plaçant une annotation de liste de classes autorisées sur une méthode ou une classe de controller

--- src/mg/itu/framework/annotation/validation/Range.java
+++ src/mg/itu/framework/annotation/validation/Range.java
@@ -5,9 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import mg.itu.framework.utils.validation.RangeValidator;
+
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-@Validation(linkedValidator = "mg.itu.framework.utils.validation.RangeValidator")
+@Validation(linkedValidator = RangeValidator.class)
 public @interface Range {
     int minValue();
     int maxValue();
---
+++ src/mg/itu/framework/utils/errors/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package mg.itu.framework.utils.errors;
+
+public class UnauthorizedException extends Exception{
+    public UnauthorizedException(String message){
+        super(message);
+    }
+}
